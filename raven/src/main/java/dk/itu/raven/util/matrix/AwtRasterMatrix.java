e568e34f32db9538b06a25a41e43d7f923977251
Mansin-ITU <beth@itu.dk>
2024-02-15 11:29:19 +0100

    removed unused code.

--- raven/src/main/java/dk/itu/raven/util/matrix/AwtRasterMatrix.java
+++ raven/src/main/java/dk/itu/raven/util/matrix/AwtRasterMatrix.java
@@ -1,44 +1,16 @@
 package dk.itu.raven.util.matrix;
 
-import java.awt.image.DataBuffer;
 import java.awt.image.Raster;
 import java.io.IOException;
 
 public class AwtRasterMatrix extends Matrix {
     private Raster raster;
-    int bytesPerPixel;
     int numberOfBands;
     int sampleSize[];
-
 
     public AwtRasterMatrix(Raster raster) {
         super(raster.getWidth(), raster.getHeight());
         this.raster = raster;
-
-        switch (raster.getSampleModel().getDataType()) {
-            case DataBuffer.TYPE_BYTE:
-                bytesPerPixel = 1;
-                break;
-            case DataBuffer.TYPE_DOUBLE:
-                bytesPerPixel = 8;
-                break;
-            case DataBuffer.TYPE_FLOAT:
-                bytesPerPixel = 4;
-                break;
-            case DataBuffer.TYPE_INT:
-                bytesPerPixel = 4;
-                break;
-            case DataBuffer.TYPE_SHORT:
-                bytesPerPixel = 2;
-                break;
-            case DataBuffer.TYPE_USHORT:
-                bytesPerPixel = 2;
-                break;
-        
-            default:
-                bytesPerPixel = 0;
-                break;
-        }
 
         sampleSize = raster.getSampleModel().getSampleSize();
         numberOfBands = raster.getNumBands();
