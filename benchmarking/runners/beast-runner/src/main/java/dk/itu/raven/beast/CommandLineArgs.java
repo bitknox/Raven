1ce561fff9f198f7b2cb4403b1f2204c7f52620f
Alexander Bilde Pedersen <alexanderbilde@live.dk>
2024-03-05 10:55:22 +0100

    implemented command line arguments for both raven runner and beast runner plotter now dows not iclude the cold starts added a jointype to raven runner to prevent magic string issues

---
+++ benchmarking/runners/beast-runner/src/main/java/dk/itu/raven/beast/CommandLineArgs.java
@@ -0,0 +1,25 @@
+package dk.itu.raven.beast;
+
+import java.io.Serializable;
+
+import com.beust.jcommander.Parameter;
+
+public class CommandLineArgs implements Serializable {
+        @Parameter(names = { "-ir", "--input-raster" }, description = "Input raster file", required = true)
+        public String inputRaster = null;
+
+        @Parameter(names = { "-iv", "--input-vector" }, description = "Input vector file", required = true)
+        public String inputVector = null;
+
+        @Parameter(names = { "-fl",
+                        "--filter-low" }, description = "Lower value for the filter range")
+        public long filterLow = (long) Integer.MIN_VALUE;
+
+        @Parameter(names = { "-fh",
+                        "--filter-high" }, description = "Upper value for the filter range")
+        public long filterHigh = (long) Integer.MAX_VALUE;
+
+        @Parameter(names = { "-i",
+                        "--iterations" }, description = "The number of tests performed using these settings", required = true)
+        public long iterations = 1;
+}
---
+++ benchmarking/runners/raven-runner/src/main/java/dk/itu/raven/runner/CommandLineArgs.java
@@ -0,0 +1,31 @@
+package dk.itu.raven.runner;
+
+import com.beust.jcommander.Parameter;
+
+public class CommandLineArgs {
+        @Parameter(names = { "-ir", "--input-raster" }, description = "Input raster file", required = true)
+        public String inputRaster = null;
+
+        @Parameter(names = { "-iv", "--input-vector" }, description = "Input vector file", required = true)
+        public String inputVector = null;
+
+        @Parameter(names = { "-fl",
+                        "--filter-low" }, description = "Lower value for the filter range")
+        public long filterLow = (long) Integer.MIN_VALUE;
+
+        @Parameter(names = { "-fh",
+                        "--filter-high" }, description = "Upper value for the filter range")
+        public long filterHigh = (long) Integer.MAX_VALUE;
+
+        @Parameter(names = { "-ts",
+                        "--tile-size" }, description = "The size of the tiles (only used if the join is streamed)")
+        public int tileSize = 2048;
+
+        @Parameter(names = { "-t",
+                        "--join-type" }, description = "The type of join to perform", required = true)
+        public JoinType joinType = null;
+
+        @Parameter(names = { "-i",
+                        "--iterations" }, description = "The number of tests performed using these settings", required = true)
+        public long iterations = -1;
+}
