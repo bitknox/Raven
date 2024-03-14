e597e75943db611bcc34e2cef1587626af8bb0a7
Alexander Bilde Pedersen <alexanderbilde@live.dk>
2024-03-14 17:41:17 +0100

    changed module in root from raptorjoin to raptor

--- benchmarking/runners/raptor-runner/src/main/java/dk/itu/raven/runner/App.java
+++ benchmarking/runners/raptor-runner/src/main/java/dk/itu/raven/runner/App.java
@@ -2,10 +2,6 @@ package dk.itu.raven.runner;
 
 import com.beust.jcommander.JCommander;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         CommandLineArgs jct = new CommandLineArgs();
