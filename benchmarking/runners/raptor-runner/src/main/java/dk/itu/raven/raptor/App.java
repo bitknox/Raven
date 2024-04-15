1cf8405062657c8c2dbeb0390010dc77517a2999
Johan Flensmark <jokf@itu.dk>
2024-04-15 14:21:35 +0200

    feat: add multitile support for raptor runner

--- benchmarking/runners/raptor-runner/src/main/java/dk/itu/raven/raptor/App.java
+++ benchmarking/runners/raptor-runner/src/main/java/dk/itu/raven/raptor/App.java
@@ -1,13 +1,15 @@
 package dk.itu.raven.raptor;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.stream.Stream;
+
+import java.util.Arrays;
+import java.util.List;
 
 import com.beust.jcommander.JCommander;
 import com.google.gson.Gson;
 
 import dk.itu.raptor.api.RaptorApi;
-import dk.itu.raptor.join.JoinResult;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -24,13 +26,37 @@ public class App {
         RaptorApi api = new RaptorApi();
         for (int i = 0; i < jct.iterations; i++) {
             long start = System.currentTimeMillis();
+            // locate tiles
+            File dir = new File(jct.inputRaster);
+            List<File> files = Arrays.asList(dir.listFiles());
 
-            Stream<JoinResult> res = api.join(jct.inputRaster, jct.inputVector);
-            if (jct.filterLow == Integer.MIN_VALUE && jct.filterHigh == Integer.MAX_VALUE) {
-                res.count();
-            } else {
-                res.filter(f -> f.m >= jct.filterLow && f.m <= jct.filterHigh).count();
-            }
+            files.stream().map(f -> {
+                try {
+                    // find tiff file in the directory
+                    if (!f.isDirectory()) {
+                        return null;
+                    }
+                    File[] tiffFiles = f.listFiles((dir1, name) -> name.endsWith(".tif"));
+
+                    if (tiffFiles.length == 0) {
+                        return null;
+                    }
+                    return api.join(tiffFiles[0].getAbsolutePath(), jct.inputVector);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }).map(res -> {
+                if (res == null) {
+                    return 0l;
+                }
+                if (jct.filterLow == Integer.MIN_VALUE && jct.filterHigh == Integer.MAX_VALUE) {
+                    return res.count();
+                } else {
+                    return res.filter(f -> f.m >= jct.filterLow && f.m <= jct.filterHigh).count();
+                }
+            }).count();
+
             long end = System.currentTimeMillis();
             long time = end - start;
             benchResult.addEntry(time);
