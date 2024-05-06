44eafdc7593b8eb02c78206d5384ad7732c7490b
Alexander Bilde Pedersen <alexanderbilde@live.dk>
2024-05-06 17:29:10 +0200

    improved perlin noise program

--- swift/raster/perlin.py
+++ swift/raster/perlin.py
@@ -9,11 +9,20 @@ import scipy.stats as st
 def rgb_norm(world):
     world_min = np.min(world)
     world_max = np.max(world)
-    norm = lambda x: np.uint8(((x - world_min) / (world_max - world_min)) * 256)
+    norm = lambda x: np.uint8(
+        min(255, ((x - world_min) / (world_max - world_min)) * 256)
+    )
     return np.vectorize(norm)
 
 
 colour_cutoffs = [35, 70, 140, 150, 200, 230, 256]
+cutoff = {}
+for x in range(256):
+    for k in range(len(colour_cutoffs)):
+        if x <= colour_cutoffs[k]:
+            cutoff[x] = k
+            break
+
 colours = [
     [0, 0, 50],
     [0, 0, 100],
@@ -27,9 +36,19 @@ colours = sum(colours, [])
 
 
 def colour_pixel(x):
-    for i in range(len(colour_cutoffs)):
-        if x <= colour_cutoffs[i]:
-            return np.uint8(i)
+    return cutoff[x]
+
+
+def convert_to_rgb(m):
+    new_m = np.zeros([m.shape[0], m.shape[1], 3])
+    for i in range(m.shape[0]):
+        for j in range(m.shape[1]):
+            value = 131072 * (cutoff[m[i][j]] + 1) - 1
+            new_m[i][j][0] = np.uint8((value >> 16) & 0xFF)
+            new_m[i][j][1] = np.uint8((value >> 8) & 0xFF)
+            new_m[i][j][2] = np.uint8((value >> 0) & 0xFF)
+
+    return new_m
 
 
 # Prep the world for saving
@@ -76,7 +95,7 @@ def generate_perlin(shape, scale, octaves, persistence, lacunarity, seed, select
     world = generate_perlin_inner(shape, scale, octaves, persistence, lacunarity, seed)
     if selectivity is None:
         world = prep_world(world)
-        img = Image.fromarray(world, mode="P")
+        img = Image.fromarray(world.astype(np.uint8), mode="P")
         img.putpalette(data=colours)
     else:
         sigma = np.std(world)
